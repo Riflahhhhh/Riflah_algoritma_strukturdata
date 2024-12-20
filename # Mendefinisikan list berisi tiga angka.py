e5902148931506f57fd14fc9846e23dc695e6516
Riflah Nasikah Elyinah <riflah028@gmail.com>
2024-12-20 09:10:17 +0700

    Add files via upload

---
+++ # Mendefinisikan list berisi tiga angka.py
@@ -0,0 +1,36 @@
+# list angka
+angka = [1, 2, 3, 4, 5]
+print("List angka:", angka)
+
+# list tiga angka
+angka = [5, 10, 15]
+print("List angka:", angka)
+# jumlah tiga angka dalam list
+angka = [5, 10, 15]
+jumlah = sum(angka)
+print("Jumlah dari ketiga angka adalah:", jumlah)
+
+# list kosong
+angka = []
+print("list kosong:", angka)
+# Menambahkan tiga angka ke dalam list
+angka.append(2)
+angka.append(4)
+angka.append(6)
+# Menampilkan list setelah penambahan angka
+print("List setelah penambahan:", angka)
+# Menghapus elemen terakhir dari list
+angka.pop()
+# Menampilkan list setelah penghapusan elemen terakhir
+print("List setelah penghapusan elemen terakhir:", angka)
+
+# Membuat string dengan nama
+nama = "Riflah Nasikah Elyinah"
+jumlah_kata = len(nama.split())
+print("String:", nama)
+print("Jumlah kata:", jumlah_kata)
+
+# Membuat tuple dengan beberapa elemen
+my_tuple = (1, 2, 3)
+print("Tuple:", my_tuple)
+
